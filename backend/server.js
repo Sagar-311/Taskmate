3feab744565c69483d94df86d73b6cc6a7775c39
Sagar-3s <1by23csbmsit.in>
2025-10-30 22:38:38 +0530

    Initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/api/test", (req, res) => {
+  res.send("TaskMate backend is running ");
+});
+
+
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
